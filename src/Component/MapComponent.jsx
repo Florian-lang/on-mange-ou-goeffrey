d92cf6ecd86a30585af7eafbb8d93b467fe5e938
florian.baumard <florian.baumard@consonanceweb.com>
2023-07-18 14:02:39 +0200

    add marker and popup for mapcomponent

--- src/Component/MapComponent.jsx
+++ src/Component/MapComponent.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import '../index.css';
 import 'leaflet/dist/leaflet.css';
 import HomeButton from './HomeButton';
+import L from "leaflet";
 
 const MapComponent = () => {
+    const position = [45.159555, 1.533937];
+
     return (
         <>
-            <MapContainer center={[45.159555, 1.533937]} zoom={13} className="h-screen w-full">
+            <MapContainer center={position} zoom={13} className="h-screen w-full">
                 <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <Marker position={position}>
+                    <Popup>
+                        Vous êtes ici
+                    </Popup>
+                </Marker>
             </MapContainer>
 
             <HomeButton/>
